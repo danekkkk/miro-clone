ff8d1377b7cfa7316f827fbad54ff1773fbcbdfc
Dominik Stwora <danek.buisness@gmail.com>
2025-05-10 23:09:59 +0200

    feat: room info section

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,9 +1,74 @@
 import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { Hint } from "@/components/hint";
+import { useRenameModal } from "@/store/use-rename-modal";
+import { Actions } from "@/components/actions";
+import { Menu } from "lucide-react";
 
-export const Info = () => {
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600", "700"],
+});
+
+const TabSeparator = () => {
+  return <div className="text-neutral-300 px-1.5">|</div>;
+};
+
+type InfoProps = {
+  boardId: string;
+};
+
+export const Info = ({ boardId }: InfoProps) => {
+  const data = useQuery(api.board.get, { id: boardId as Id<"boards"> });
+  const { onOpen } = useRenameModal();
+
+  if (!data) return <Info.Skeleton />;
+
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-      TODO: Information about the board
+      <Hint label="Go to boards" side="bottom" sideOffset={10}>
+        <Button asChild variant="board" className="px-2">
+          <Link href="/">
+            <Image src="/logo.svg" alt="Logo" width={40} height={40} />
+            <span
+              className={cn("font-semibold text-xl text-black", font.className)}
+            >
+              Miro
+            </span>
+          </Link>
+        </Button>
+      </Hint>
+
+      <TabSeparator />
+
+      <Hint label="Edit title" side="bottom" sideOffset={10}>
+        <Button
+          variant="board"
+          className="text-base font-normal px-2"
+          onClick={() => onOpen(data._id, data.title)}
+        >
+          {data.title}
+        </Button>
+      </Hint>
+
+      <TabSeparator />
+
+      <Actions id={data._id} title={data.title} side="bottom" sideOffset={10}>
+        <div className="cursor-pointer">
+          <Hint label="Main menu" side="bottom" sideOffset={10}>
+            <Button variant="board" className="p-1">
+              <Menu />
+            </Button>
+          </Hint>
+        </div>
+      </Actions>
     </div>
   );
 };
--- app/board/[boardId]/_components/loading.tsx
+++ app/board/[boardId]/_components/loading.tsx
@@ -2,6 +2,7 @@ import { Loader } from "lucide-react";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
+
 export const Loading = () => {
   return (
     <main className="size-full relative bg-neutral-100 touch-none flex items-center justify-center">
