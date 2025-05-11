36a54b81429ad338299868639bae83a474373739
Dominik Stwora <danek.buisness@gmail.com>
2025-05-11 13:17:08 +0200

    feat: change layer depth

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -8,7 +8,7 @@ import { ColorPicker } from "./color-picker";
 import { useDeleteLayers } from "@/hooks/use-delete-layers";
 import { Button } from "@/components/ui/button";
 import { Hint } from "@/components/hint";
-import { Trash2 } from "lucide-react";
+import { BringToFront, SendToBack, Trash2 } from "lucide-react";
 
 type SelectionToolsProps = {
   camera: Camera;
@@ -18,6 +18,51 @@ type SelectionToolsProps = {
 export const SelectionTools = memo(
   ({ camera, setLastUsedColor }: SelectionToolsProps) => {
     const selection = useSelf((me) => me.presence.selection);
+
+    const moveToBack = useMutation(
+      ({ storage }) => {
+        const liveLayersIds = storage.get("layerIds");
+
+        const indices: number[] = [];
+
+        const arr = liveLayersIds.toArray();
+
+        for (let i = 0; i < arr.length; i++) {
+          if (selection.includes(arr[i])) {
+            indices.push(i);
+          }
+        }
+
+        for (let i = 0; i < indices.length; i++) {
+          liveLayersIds.move(indices[i], i);
+        }
+      },
+      [selection]
+    );
+
+    const bringToFront = useMutation(
+      ({ storage }) => {
+        const liveLayersIds = storage.get("layerIds");
+
+        const indices: number[] = [];
+
+        const arr = liveLayersIds.toArray();
+
+        for (let i = 0; i < arr.length; i++) {
+          if (selection.includes(arr[i])) {
+            indices.push(i);
+          }
+        }
+
+        for (let i = indices.length - 1; i >= 0; i--) {
+          liveLayersIds.move(
+            indices[i],
+            arr.length - 1 - (indices.length - 1 - i)
+          );
+        }
+      },
+      [selection]
+    );
 
     const setFill = useMutation(
       ({ storage }, fill: Color) => {
@@ -50,10 +95,25 @@ export const SelectionTools = memo(
         }}
       >
         <ColorPicker onChange={setFill} />
+
+        <div className="flex flex-col gap-y-0.5">
+          <Hint label="Bring to front">
+            <Button variant="board" size="icon" onClick={bringToFront}>
+              <BringToFront />
+            </Button>
+          </Hint>
+
+          <Hint label="Send backward">
+            <Button variant="board" size="icon" onClick={moveToBack}>
+              <SendToBack />
+            </Button>
+          </Hint>
+        </div>
+
         <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
           <Hint label="Delete">
             <Button variant="board" size="icon" onClick={deleteLayers}>
-              <Trash2 />
+              <Trash2 color="#F35223" />
             </Button>
           </Hint>
         </div>
