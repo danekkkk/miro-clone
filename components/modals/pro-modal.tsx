b6c2783605a37f8db037e6499caa19aee1a5905c
Dominik Stwora <danek.buisness@gmail.com>
2025-05-17 21:34:33 +0200

    feat: pro modal

--- components/modals/pro-modal.tsx
+++ components/modals/pro-modal.tsx
@@ -1,27 +1,47 @@
 "use client";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
-
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import Image from "next/image";
 import { useProModal } from "@/store/use-pro-modal";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export const ProModal = () => {
   const { isOpen, onClose } = useProModal();
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Upgrade to Pro</DialogTitle>
-          <DialogDescription>
-            Create unlimited boards and more
-          </DialogDescription>
-        </DialogHeader>
+      <DialogContent className="max-w-[340px] p-0 overflow-hidden">
+        <div className="aspect-video relative flex items-center justify-center">
+          <Image src="/pro.svg" alt="Pro image" className="object-fit" fill />
+        </div>
+
+        <div
+          className={cn(
+            "text-neutral-700 mx-auto space-y-6 p-6",
+            font.className
+          )}
+        >
+          <h2 className="font-medium text-lg">🚀 Upgrade to Pro today!</h2>
+          <div className="pl-3">
+            <ul className="text-[11px] space-y-1 list-disc">
+              <li>Unlimited boards</li>
+              <li>Unlimited organizations</li>
+              <li>Unlimited members</li>
+              <li>and much more!</li>
+            </ul>
+          </div>
+
+          <Button size="sm" className="w-full">
+            Upgrade to pro
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
